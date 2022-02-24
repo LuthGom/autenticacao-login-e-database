25a887aa29b9ead700d757830fdaf6be135f20be
LuthGom <lucianomendes23@yahoo.com>
2022-02-24 11:37:48 -0300

    revertendo commits2

--- backEnd/src/controller/usuarioController.js
+++ backEnd/src/controller/usuarioController.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
     app
         .get('/', async (req, res) => {
             try {
+                // const listaDeUsuarios = await Usuarios.listarTodosOsUsuarios();
                 res.status(200).json({
                     usuario: 'magalu',
                     senha: 'm@galu123'
@@ -16,13 +17,14 @@ module.exports = (app) => {
         .post('/login', (req, res) => {
             try {
                 const {usuario, senha} = req.body;
+                // console.log(bd.usuario, bd.senha);
                 console.log(usuario, senha);
                 if(usuario === bd.usuario && senha === bd.senha) {
                     return res.status(200).json({usuario, senha})
                 }
-                 else {
-                    return res.status(500).json('usuario ou senha incorretos')
-                }
+                //  else {
+                //     return res.status(500).json('usuario ou senha incorretos')
+                // }
             } catch (erro) {
                 console.log(erro);
                 return res.status(500).json({erro: erro.message})
